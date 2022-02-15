2e486ba0b41dbdefd991ae742edc3f23fca42223
yakupbilgen <yakupbilgen92@gmail.com>
2022-02-15 17:15:39 +0300

    create project

---
+++ android/app/src/main/kotlin/org/bilgen/cloud_firestore_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.bilgen.cloud_firestore_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore_example/screen/homepade.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Homepage(),
+  ));
+}
