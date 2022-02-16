55242b02ba5787c23501f3777c60dcee6a46d26c
yakupbilgen92@gmail.com <yakupbilgen92@gmail.com>
2022-02-16 17:18:21 +0300

    firebase add user

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ import './screen/homepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
