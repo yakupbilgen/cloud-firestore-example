a1650fa9a5e50229838ec360d25e596838032b60
yakupbilgen92@gmail.com <yakupbilgen92@gmail.com>
2022-02-16 17:18:10 +0300

    firebase add user

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -15,6 +16,28 @@ class _HomepageState extends State<Homepage> {
       body: const Center(
         child: Text('data'),
       ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: _createUserEmailAndPassword,
+        child: Icon(Icons.add),
+      ),
     );
+  }
+}
+
+Future<void> _createUserEmailAndPassword() async {
+  try {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+            email: "example@email.com", password: "123456");
+
+    debugPrint("email: ${userCredential.user?.email}");
+  } on FirebaseAuthException catch (e) {
+    if (e.code == "weak-password") {
+      debugPrint("The password provided is to weak.");
+    } else if (e.code == "email-already-in-use") {
+      debugPrint("The account already exists for the email.");
+    }
+  } catch (e) {
+    debugPrint(e.toString());
   }
 }
