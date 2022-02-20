97dc736f2918b58eaa1aaa9f5a3709cf60f35a50
yakupbilgen <yakupbilgen92@gmail.com>
2022-02-20 23:46:20 +0300

    login screen

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,3 +1,4 @@
 class AppConstant {
   static const defaultPadding = 20.0;
+  static const defaultHeight = 20.0;
 }
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constant/constant.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -10,10 +12,32 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-        padding: EdgeInsets.all(value),
+        padding: const EdgeInsets.all(AppConstant.defaultPadding),
+        child: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'E-mail'),
+            ),
+            const SizedBox(height: AppConstant.defaultHeight),
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Password'),
+            ),
+            const SizedBox(height: AppConstant.defaultHeight),
+            Container(
+              width: deviceSize.width,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Login'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
