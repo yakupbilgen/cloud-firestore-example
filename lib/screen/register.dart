c74adb535799b8ef7e08d352a2a69d0eea589a0f
yakupbilgen <yakupbilgen92@gmail.com>
2022-02-21 23:47:11 +0300

    register UI

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constant/constant.dart';
+
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
@@ -10,6 +12,33 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size deviceSize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: const EdgeInsets.all(AppConstant.defaultPadding),
+        child: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'E-mail'),
+            ),
+            const SizedBox(height: AppConstant.defaultHeight),
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Password'),
+            ),
+            const SizedBox(height: AppConstant.defaultHeight),
+            Container(
+              width: deviceSize.width,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Login'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
