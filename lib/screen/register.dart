4f9e4d98d36dae72782c60ab9daa74998a4fa7a4
yakupbilgen <yakupbilgen92@gmail.com>
2022-02-23 21:53:52 +0300

    register screen UI

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -20,13 +20,25 @@ class _RegisterScreenState extends State<RegisterScreen> {
         child: Column(
           children: [
             const TextField(
+              keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'E-mail'),
+                border: OutlineInputBorder(),
+                hintText: 'E-mail',
+                filled: true,
+                fillColor: Color.fromARGB(99, 189, 189, 189),
+                prefix: Icon(Icons.person),
+              ),
             ),
             const SizedBox(height: AppConstant.defaultHeight),
             const TextField(
+              keyboardType: TextInputType.visiblePassword,
               decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Password'),
+                border: OutlineInputBorder(),
+                hintText: 'Password',
+                suffixIcon: Icon(Icons.remove_red_eye),
+                filled: true,
+                fillColor: Color.fromARGB(99, 189, 189, 189),
+              ),
             ),
             const SizedBox(height: AppConstant.defaultHeight),
             Container(
